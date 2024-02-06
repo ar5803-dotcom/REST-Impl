64f71aba74f51e61d0c344819fdfa36e80a16855
AAYUSH KUMAR <92009451+ar5803-dotcom@users.noreply.github.com>
2024-02-06 21:55:49 +0530

    Update DepartmentRepository.java

--- Spring-boot-tut/src/main/java/com/dailybuffer/Springboot/tut/repository/DepartmentRepository.java
+++ Spring-boot-tut/src/main/java/com/dailybuffer/Springboot/tut/repository/DepartmentRepository.java
@@ -6,4 +6,8 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface DepartmentRepository extends JpaRepository<Department,Long> {
+  
+    public Department findByDepartmentName(String departmentName);
+
+    public Department findByDepartmentNameIgnoreCase(String departmentName);
 }
