f3e37ec37cef8fee694bd64536b16abff49b46b1
AAYUSH KUMAR <92009451+ar5803-dotcom@users.noreply.github.com>
2024-02-21 00:36:33 +0530

    Add files via upload

---
+++ Spring-boot-tut/src/main/java/com/dailybuffer/Springboot/tut/entity/ErrorMessage.java
@@ -0,0 +1,16 @@
+package com.dailybuffer.Springboot.tut.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorMessage {
+    private HttpStatus status;
+    private String message;
+
+    public ErrorMessage(HttpStatus httpStatus) {
+    }
+}
